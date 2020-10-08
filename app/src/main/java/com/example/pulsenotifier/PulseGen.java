994bb95beebafe1f0a42c4b4479c991628ca0723
chen goldi <17chen07@gmail.com>
2020-10-08 19:41:16 +0300

    not final pulse class

--- app/src/main/java/com/example/pulsenotifier/PulseGen.java
+++ app/src/main/java/com/example/pulsenotifier/PulseGen.java
@@ -1,4 +1,6 @@
 package com.example.pulsenotifier;
+import android.os.SystemClock;
+
 import java.util.Random;
 
 
@@ -6,15 +8,17 @@ public class PulseGen {
     private int currentPulse;
     private boolean alive = true;
     private boolean stressed = false;
-    private static final int min_pulse = 20;
+//    private static final int min_pulse = 20;
     private static final int normal_pulse_resting = 80;
-    private static final int max_pulse = 220;
+    private static final int normal_pulse_deviation = 30;
+    private static final int stress_pulse_deviation = 15;
+//    private static final int max_pulse = 220;
     private int currentMeanPulse;
     private int currentStdPulse;
 
     public PulseGen() {
         this.currentPulse = normal_pulse_resting;
-        this.currentMeanPulse = 80;
+        this.currentMeanPulse = normal_pulse_resting;
         this.currentStdPulse = 30;
     }
 
@@ -25,17 +29,27 @@ public class PulseGen {
 
 
     public void bitPulse() {
-        int newPulse;
         Random rand = new Random();
         while(alive){
-            newPulse = (int)(rand.nextGaussian()*this.currentStdPulse + this.currentMeanPulse);
-            if (!stressed && newPulse > 120 && (int)rand.nextInt(100) >= 50){
+            this.currentPulse = (int)(rand.nextGaussian()*this.currentStdPulse + this.currentMeanPulse);
+            if (!stressed && this.currentPulse > 120 && (int)rand.nextInt(100) >= 50){
                     this.currentMeanPulse = 120;
-                    this.currentStdPulse = 15;
+                    this.currentStdPulse = stress_pulse_deviation;
                     this.stressed = true;
             }
 
+            if (!stressed && this.currentPulse < 50 && (int)rand.nextInt(101) >= 50){
+                this.currentMeanPulse = 50;
+                this.currentStdPulse = stress_pulse_deviation;
+                this.stressed = true;
+            }
 
+            if(stressed && this.currentPulse <= 120 && this.currentPulse >= 50){
+                stressed = false;
+                this.currentMeanPulse = normal_pulse_resting;
+                this.currentStdPulse = normal_pulse_deviation;
+            }
+            SystemClock.sleep(1000);
         }
     }
 }
